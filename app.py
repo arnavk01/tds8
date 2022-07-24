e085c1d8fe398bb4c79349df77228a4d17c86a77
arnavk01 <109910130+arnavk01@users.noreply.github.com>
2022-07-25 01:20:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ st.write("""
 
 st.header('Please Enter the 3 numbers below')
 
-num1 = st.number_input('Num1')
-num2 = st.number_input('Num2')
-num3 = st.number_input('Num3')
+num1 = st.number_input('First Number:')
+num2 = st.number_input('Second Number:')
+num3 = st.number_input('Third Number:')
 
 greatest_num = max(num1, num2, num3)
 st.subheader('The largest number is :')
