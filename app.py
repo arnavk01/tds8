aeefb74e662745a3494ba8a50cb4ac38d5e7b169
arnavk01 <109910130+arnavk01@users.noreply.github.com>
2022-07-24 23:29:02 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+
+st.write("""
+# : Find largest among 3 given numbers
+""")
+
+st.header('User Input Parameters')
+
+num1 = st.number_input('Num1')
+num2 = st.number_input('Num2')
+num3 = st.number_input('Num3')
+
+greatest_num = max(num1, num2, num3)
+st.write(greatest_num)
