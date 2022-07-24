f5bfd212f01e431e96476f5e37ab18edd29c514c
arnavk01 <109910130+arnavk01@users.noreply.github.com>
2022-07-25 01:11:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 
 st.write("""
-# : Find largest among 3 given numbers
+# Find largest among 3 given numbers
 """)
 
 st.header('User Input Parameters')
@@ -12,4 +12,5 @@ num2 = st.number_input('Num2')
 num3 = st.number_input('Num3')
 
 greatest_num = max(num1, num2, num3)
+st.subheader('the largest number is :')
 st.write(greatest_num)
