cd79755af798b40799daddc8cb5568ec75992d57
arnavk01 <109910130+arnavk01@users.noreply.github.com>
2022-07-25 01:15:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,12 @@ st.write("""
 # Find largest among 3 given numbers
 """)
 
-st.header('User Input Parameters')
+st.header('Please Enter the 3 numbers below')
 
 num1 = st.number_input('Num1')
 num2 = st.number_input('Num2')
 num3 = st.number_input('Num3')
 
 greatest_num = max(num1, num2, num3)
-st.subheader('the largest number is :')
+st.subheader('The largest number is :')
 st.write(greatest_num)
